cbf26e7fe807ca9ab545035413e410481fc6cf0c
Alan Sławiński <sokokoldproductions@gmail.com>
2024-04-20 12:29:08 +0200

    update

---
+++ src/BaseAbstract.java
@@ -0,0 +1,12 @@
+abstract class BaseAbstract {
+        private String surName = "Lewandowski";
+
+        public String getSurName() {
+            return surName;
+        }
+
+        public void setSurName(String surName) {
+            this.surName = surName;
+        }
+    }
+
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        Car car = new Car();
+        System.out.println(car.InfoCar());
+
+        Car car2 = new Car("Różowy");
+        System.out.println(car2.InfoCar());
+
+        Car car3 = new Car("Czarny", 2024);
+        System.out.println(car3.InfoCar());
+
+    }
+}
